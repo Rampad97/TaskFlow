401aab1d98e0a5901b85b937a9b3cf0615f75fa6
Fernanda <fernandarampad97@gmail.com>
2025-07-30 22:02:08 -0600

    feat: crear repositorios JpaRepository para Usuario, Proyecto y Tarea

---
+++ src/main/java/com/meta/TaskFlow/repositories/TareaRepository.java
@@ -0,0 +1,11 @@
+package com.meta.TaskFlow.repositories;
+
+import com.meta.TaskFlow.entities.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TareaRepository extends JpaRepository<Tarea, Integer> {
+    List<Tarea> findByUsuarioId(Integer usuarioId);
+    List<Tarea> findByProyectId(Integer proyectoId);
+}

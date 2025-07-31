2cb362e6037877d1dc94bcc02e8a42a6ce3bde11
Fernanda <fernandarampad97@gmail.com>
2025-07-30 23:22:25 -0600

    feat: agregar controladores REST para Usuario, Proyecto y Tarea

---
+++ src/main/java/com/meta/TaskFlow/controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.meta.TaskFlow.controllers;
+
+import com.meta.TaskFlow.entities.Usuario;
+import com.meta.TaskFlow.services.interfaces.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/TaskFlow" +
+        "/usuarios")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    // Crear nuevo usuario
+    @PostMapping
+    public Usuario createUser(@RequestBody Usuario usuario) {
+        return usuarioService.newUser(usuario);
+    }
+
+    // Lista de todos los usuarios
+    @GetMapping
+    public List<Usuario> allUsers() {
+        return  usuarioService.getAllUsers();
+    }
+
+    // Buscar usuario por ID
+    @GetMapping("/{id}")
+    public Optional<Usuario> userById(@PathVariable Integer id) {
+        return  usuarioService.getUserById(id);
+    }
+}

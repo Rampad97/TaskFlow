f1b50306b7e83f6260f1280072e7ece86249d16c
Fernanda <fernandarampad97@gmail.com>
2025-07-31 13:15:17 -0600

    feat: Agregar método para actualizar estado de una tarea en Service y Controller

--- src/main/java/com/meta/TaskFlow/services/impl/TareaServiceImpl.java
+++ src/main/java/com/meta/TaskFlow/services/impl/TareaServiceImpl.java
@@ -1,11 +1,13 @@
 package com.meta.TaskFlow.services.impl;
 
+import com.meta.TaskFlow.entities.Estado;
 import com.meta.TaskFlow.entities.Tarea;
 import com.meta.TaskFlow.repositories.TareaRepository;
 import com.meta.TaskFlow.services.interfaces.TareaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.lang.model.util.ElementScanner6;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,17 +32,25 @@ public class TareaServiceImpl implements TareaService {
     }
 
     @Override
-    public List<Tarea> getTasksbyUserId(Integer usuarioId) {
+    public List<Tarea> getTasksByUserId(Integer usuarioId) {
         return tareaRepository.findByUsuarioId(usuarioId);
     }
 
     @Override
-    public List<Tarea> getTaskbyProjectoId(Integer proyectoId) {
+    public List<Tarea> getTaskByProjectId(Integer proyectoId) {
         return tareaRepository.findByProyectoId(proyectoId);
     }
 
     @Override
     public void deleteTask(Integer id) {
         tareaRepository.deleteById(id);
+    }
+
+    @Override
+    public Tarea updateStatus(Integer tareaId, Estado nuevoEstado) {
+        Tarea tarea = tareaRepository.findById(tareaId)
+                .orElseThrow(()-> new IllegalArgumentException("Tarea con ID: " + tareaId + " no encontrada"));
+        tarea.setEstado(nuevoEstado);
+        return tareaRepository.save(tarea);
     }
 }

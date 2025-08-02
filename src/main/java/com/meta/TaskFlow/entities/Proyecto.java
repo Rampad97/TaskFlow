3e770891d2cc04dd100aebe02e733749a744c1aa
Fernanda <fernandarampad97@gmail.com>
2025-08-02 10:12:38 -0600

    refactor(entidades): Estado y prioridades enums. Se reemplazaron a clases: Rol, Usuario, Proyecto y Tarea)

--- src/main/java/com/meta/TaskFlow/entities/Proyecto.java
+++ src/main/java/com/meta/TaskFlow/entities/Proyecto.java
@@ -3,6 +3,7 @@ package com.meta.TaskFlow.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "proyecto")
@@ -11,25 +12,23 @@ public class Proyecto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank(message = "El nombre del proyecto es obligatorio")
-    private String nombre;
-
-    private String descripcion;
+    @NotBlank(message = "El titulo del proyecto es obligatorio")
+    @Size(min = 3, max = 150, message = "El titulo del proyecto debe tener entre 3 y 150 caracteres")
+    @Column(unique = true)
+    private String titulo;
 
     @NotNull(message = "Debe asignarse un lider al proyecto")
-    @ManyToOne
-    @JoinColumn(name = "lider_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "lider_id", nullable = false)
     private Usuario lider;
 
     // Contructores
-
     public Proyecto() {
     }
 
-    public Proyecto(Integer id, String nombre, String descripcion, Usuario lider) {
+    public Proyecto(Integer id, String titulo, Usuario lider) {
         this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
+        this.titulo = titulo;
         this.lider = lider;
     }
 
@@ -43,20 +42,12 @@ public class Proyecto {
         this.id = id;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getTitulo() {
+        return titulo;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public Usuario getLider() {

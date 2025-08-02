8e59aa8f5e3f7b8597ae0517abdb8108a96e3551
Fernanda <fernandarampad97@gmail.com>
2025-08-02 10:31:23 -0600

    feature(repositories): mejora de logica de cada repositorio

--- src/main/java/com/meta/TaskFlow/repositories/ProyectoRepository.java
+++ src/main/java/com/meta/TaskFlow/repositories/ProyectoRepository.java
@@ -3,6 +3,10 @@ package com.meta.TaskFlow.repositories;
 import com.meta.TaskFlow.entities.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProyectoRepository extends JpaRepository<Proyecto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProyectoRepository extends JpaRepository<Proyecto, Integer> {
+    List<Proyecto> findByLiderId(Integer liderId);
+    Optional<Proyecto> findByTitulo(String titulo);
 }
